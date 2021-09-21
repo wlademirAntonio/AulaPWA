b924830f3c65211db3f6937fdfb6736b02d1142d
wlademirAntonio <85811422+wlademirAntonio@users.noreply.github.com>
2021-09-21 07:07:24 +0000

    Atualização do projeto

---
+++ sw.js
@@ -0,0 +1,20 @@
+var cacheName = 'NomeApp-v';
+var filesToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+self.addventListener('install', function(e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+self.addventListener('fetch', function(e) {
+    e.respondWith(
+        caches.match(e.request).then(function(reponse) {
+            return response || fetch(e.request);
+        })
+    );
+});
